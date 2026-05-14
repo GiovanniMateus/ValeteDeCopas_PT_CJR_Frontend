a998e9f9e8c2ba6bce531021286e0d73733d15fb
RodrigoDutraF88 <rodrigodutraf88@gmail.com>
2026-05-14 19:51:12 -0300

    feat: adiciona estrutura inicial da pagina, png e importa navbar

---
+++ app/user_profile/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import Image from "next/image";
+import Navbar from "../components/navbar";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+
+      <Navbar />
+      <section className="w-full bg-black h-[447px] flex items-center justify-between px-10 overflow-hidden">
+      </section>
+
+
+
+      <section className="bg-white min-h-[400px]" />
+
+    </main>
+  );
+}
