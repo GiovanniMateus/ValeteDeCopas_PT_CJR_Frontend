cf7b5ea7019e31ac0cdfde8829447fde6d186f5b
RodrigoDutraF88 <rodrigodutraf88@gmail.com>
2026-05-27 18:21:12 -0300

    Revert "Merge pull request #16 from GiovanniMateus/revert-15-filtro-categoria"
    This reverts commit f5355a14ae72cb3cee2f88b1acf6b42c81d1a752, reversing
    changes made to cbe8d0aee4b5d3cd3a9b3d7a4beee2b2f7035c2e.

---
+++ app/categoria_especifica/page.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import Image from "next/image";
+import Navbar from "../components/navbar4";
+import Pesquisa from "../components/pesquisa";
+import CarrosselProdutos from "../components/carrossel_produtos";
+
+
+import {
+  Shirt,
+  Gamepad2,
+  House,
+  Smartphone,
+  Pill,
+  ShoppingBasket,
+  Sparkles,
+  ToyBrick
+} from "lucide-react";
+
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+
+      <Navbar />
+
+      <section className="w-full bg-black h-[447px] flex items-center justify-between px-10 overflow-hidden">
+
+        <div className="text-white pl-40">
+          <h1 className="text-6xl font-bold ">
+            O universo da tecnologia<br />em um só lugar
+          </h1>
+        </div>
+
+        <div className="relative w-[300px] h-[400px] mr-30 mt-20">
+          <Image
+            src="/personagem_categoria_especifica.png"
+            alt="PersonagemCategoriaEspecifica"
+            fill
+            className="object-cover object-[50%_0%]"
+            sizes="(max-width: 400px) 100vw, 572px"
+            priority
+          />
+        </div>
+
+      </section>
+
+
+      <div className="bg-[#F6F3E4] min-h-[400px] px-6 py-8">
+        <div className="flex justify-end">
+          <Pesquisa />
+        </div>
+ 
+        <CarrosselProdutos
+          titulo="Melhores avaliados"
+          endpoint="/produtos/melhores-avaliados"
+        />
+
+        <CarrosselProdutos
+          titulo="Mais baratos"
+          endpoint="/produtos/mais-baratos"
+        />
+
+        <CarrosselProdutos
+          titulo="Recém adicionados"
+          endpoint="/produtos/recem-adicionados"
+        />
+      </div>
+
+    </main>
+  );
+}
